d036c20cd08845ea4889dec1e6d81efea692e80c
Eduardo Castellini <eduardo.castellini@tecnospeed.com.br>
2019-08-05 15:52:50 -0300

    Criando Rota Users, e alguns ajustes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Bem Vindo a API MyMoney' });
 });
+
+routes.post('/users', UserController.store);
 
 export default routes;
