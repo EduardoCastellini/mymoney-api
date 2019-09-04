63f916c6dc557b86c47be601cb7a7215e842e4cb
Eduardo Castellini <eduardo.castellini@hotmail.com>
2019-09-04 20:43:16 -0300

    ajusta middleware auth

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,6 +1,8 @@
 import jwt from 'jsonwebtoken';
 import { promisify } from 'util';
 
+import User from '../models/User';
+
 import authConfig from '../../config/auth';
 
 export default async (req, res, next) => {
@@ -15,10 +17,16 @@ export default async (req, res, next) => {
   try {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
+    const registeredUser = await User.findByPk(decoded.id);
+
+    if (!registeredUser) {
+      return res.status(401).json({ error: 'User not registred!' });
+    }
+
     req.userId = decoded.id;
 
     return next();
   } catch (err) {
-    return res.status(401).json({ error: 'Token not provided' });
+    return res.status(401).json({ error: 'Token not provided!' });
   }
 };
